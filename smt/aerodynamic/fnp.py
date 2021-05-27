612f7e18ad2b801265c16a8d5f64b6fdb4444a31
haojia632 <haojia632@bit.edu.cn>
2021-05-27 22:41:59 +0800

    添加气动力预测模块

--- smt/aerodynamic/fnp.py
+++ smt/aerodynamic/fnp.py
@@ -3,7 +3,6 @@
 这里是法向力预测的模型
 
 """
+from smt.aerodynamic.fp_base import FpBase
 
 class FnpModel(FpBase):
-
-
--- smt/aerodynamic/fp_base.py
+++ smt/aerodynamic/fp_base.py
@@ -4,6 +4,7 @@
 这里是气动力预测的基类，所有预测气动力的模型全部都继承自这个类
 
 """
+from smt.surrogate_models.surrogate_model import SurrogateModel
 
 class FpBase(SurrogateModel):
 
